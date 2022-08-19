a013eb450b5980b3a4bad734cc3d46dd6360b70a
Yun Jang <jang7j@yahoo.com>
2022-08-18 23:15:54 -0500

    initial commit

---
+++ Car/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{ 
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+        public Car(string make, string model, int year)
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+       
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+     }
+}
+
---
+++ Car/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace Car
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Escape";
+            myCar.Year = 2013;
+
+            var mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX5",
+                Year = 2009
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+            var carlist = new List<Car>() { myCar, mazda, chevy };
+
+            foreach (var car in carlist)
+            {
+                Console.WriteLine(car.Make);
+                Console.WriteLine(car.Model);
+                Console.WriteLine(car.Year);
+            }
+
+
+        }
+
+            
+    }
+}
